976c1fcbdcc964a36abdafe17635105bd0e3f0f3
JefferDevop <somadesarrollo@gmail.com>
2025-07-25 23:48:35 -0500

    Cambio de ruta server

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,7 @@
-export const BASE_API = "https://naciotexdistribuidora.catalogointeractivo.com.co"
-// export const BASE_API = "http://testing.localhost:8000"
+
+ export const BASE_API = "https://naciotexdistribuidora.conexion.bar"
+// export const BASE_API = "http://desarrollo.localhost:8001"
+
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart";
 export const BASE_NAME = "https://res.cloudinary.com/dnio5vufj/"
